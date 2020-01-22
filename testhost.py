8bcf4b7cc818982a6a17467720156713b548ef73
baduserlam <39348545+baduserlam@users.noreply.github.com>
2020-01-22 20:20:15 +0300

    Add files via upload

---
+++ testhost.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(pass_context=True)
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+token = os.environ.get('BOT_TOKEN')
